486eda5fc82af069d699c7a38eb47c93aed17028
Firstsup <45390884+Firstsup@users.noreply.github.com>
2020-12-02 09:12:14 +0800

    N6Q2

---
+++ src/N6/N6Q1.java
@@ -0,0 +1,28 @@
+package N6;
+
+import java.util.Stack;
+
+public class N6Q1 {
+    public static void main(String[] args) {
+        Node node5 = new Node(5, null);
+        Node node4 = new Node(4, node5);
+        Node node3 = new Node(3, node4);
+        Node node2 = new Node(2, node3);
+        Node node1 = new Node(1, node2);
+        Node head = new Node(-1, node1);
+        Stack<Integer> stack = function(head);
+        while (!stack.empty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+
+    public static Stack<Integer> function(Node head) {
+        Stack<Integer> stack = new Stack<>();
+        Node node = head;
+        while (node.getNext() != null) {
+            node = node.getNext();
+            stack.push(node.getData());
+        }
+        return stack;
+    }
+}
---
+++ src/N6/N6Q2.java
@@ -0,0 +1,22 @@
+package N6;
+
+public class N6Q2 {
+    public static void main(String[] args) {
+        Node node5 = new Node(5, null);
+        Node node4 = new Node(4, node5);
+        Node node3 = new Node(3, node4);
+        Node node2 = new Node(2, node3);
+        Node node1 = new Node(1, node2);
+        Node head = new Node(-1, node1);
+        function(head.getNext());
+    }
+
+    public static void function(Node node) {
+        if (node != null) {
+            if (node.getNext() != null) {
+                function(node.getNext());
+            }
+            System.out.print(node.getData() + " ");
+        }
+    }
+}
