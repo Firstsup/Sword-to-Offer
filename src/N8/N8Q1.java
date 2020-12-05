000a6927fa33a89bc1beb7a7e6647495bedce0d4
Firstsup <1256207041@qq.com>
2020-12-05 10:35:57 +0800

    N5Q2

--- src/N8/N8Q1.java
+++ src/N8/N8Q1.java
@@ -5,6 +5,11 @@ public class N8Q1 {
         int[] preorder = new int[]{6, 2, 1, 4, 3, 5, 8, 7, 9};
         int[] inorder = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
         Node root = buildtree(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
+        buildparent(root);
+        System.out.println(function(root.getLeft()).getValue());
+        System.out.println(function(root.getLeft().getLeft()).getValue());
+        System.out.println(function(root.getLeft().getRight().getRight()).getValue());
+        System.out.println(function(root.getRight().getRight()).getValue());
     }
 
     public static Node buildtree(int[] preorder, int[] inorder, int prestart, int preend, int instart, int inend) {
@@ -28,5 +33,43 @@ public class N8Q1 {
             node.setRight(buildtree(preorder, inorder, prestart + leftlength + 1, preend, root + 1, inend));
         }
         return node;
+    }
+
+    public static void buildparent(Node node) {
+        if (node.getLeft() != null) {
+            node.getLeft().setParent(node);
+            buildparent(node.getLeft());
+        }
+        if (node.getRight() != null) {
+            node.getRight().setParent(node);
+            buildparent(node.getRight());
+        }
+    }
+
+    public static Node function(Node node) {
+        if (node.getRight() != null) {
+            node = node.getRight();
+            while (node.getLeft() != null) {
+                node = node.getLeft();
+            }
+            return node;
+        } else {
+            if (node.getParent().getLeft() == node) {
+                return node.getParent();
+            } else if (node.getParent().getRight() == node) {
+                while (node.getParent().getLeft() != node) {
+                    node = node.getParent();
+                    if (node.getParent() == null) {
+                        break;
+                    }
+                }
+                if (node.getParent() != null) {
+                    return node.getParent();
+                }
+            }
+        }
+        Node nodenull = new Node();
+        nodenull.setValue(0);
+        return nodenull;
     }
 }
