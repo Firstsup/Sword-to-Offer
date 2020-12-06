197d847ba299be7ddd0b907410029c376136b7ee
Firstsup <45390884+Firstsup@users.noreply.github.com>
2020-12-06 17:35:24 +0800

    N10Q1

---
+++ src/N10/N10Q1.java
@@ -0,0 +1,20 @@
+package N10;
+
+import java.util.ArrayList;
+
+public class N10Q1 {
+    public static void main(String[] args) {
+        System.out.println(function(20));
+        System.out.println(function(100));
+    }
+
+    public static long function(int n) {
+        ArrayList<Long> arr = new ArrayList<>();
+        arr.add((long) 0);
+        arr.add((long) 1);
+        for (int i = 2; i <= n; i++) {
+            arr.add(arr.get(i - 2) + arr.get(i - 1));
+        }
+        return arr.get(n);
+    }
+}
